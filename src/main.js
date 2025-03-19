b5f8a890d04f1bb6ef1eba2948e0af370dc91f48
WeiLin Yu <wyu58@ucsc.edu>
2025-03-18 23:02:55 -0700

    Finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+//Name: Weilin Yu
+//Date: 3/18/2025
+//Citation: All artworks are from Weilin Yu. All sound effects and bgms are from https://pixabay.com/music/search/genre/video%20games/
+//Phaser's major components: Physics system, Camera, text objects, the animation manager, the tween manager, timers, tilemaps
+//Time spent: 40-45 hours
+
+
 
 let config = {
     type: Phaser.AUTO,
